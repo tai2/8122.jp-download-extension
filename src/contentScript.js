2a4016f05879778bb156697b5c4e1c8458582ee3
tai2 <hello@tai2.net>
2022-03-21 17:07:50 +0900

    Use fetch to download files

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,26 +1,52 @@
 main()
 
-function delay(milliseconds) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, milliseconds)
-    })
+function download(blob, filename) {
+    const a = document.createElement('a')
+    a.style = 'display: none'
+    document.body.appendChild(a)
+    const objectUrl = window.URL.createObjectURL(blob)
+    a.href = objectUrl
+    a.download = filename
+    a.click()
+    window.URL.revokeObjectURL(objectUrl)
 }
 
-async function dispatchClickToAllAllDownloadButtons() {
+function collectDownloadUrls() {
+    const downloadUrls = []
+
     const iter = document.evaluate(
-        '//*[text()="ダウンロードする"]',
+        '//a[text()="ダウンロードする"]',
         document.getElementById('downloadList'),
         null,
         XPathResult.ANY_TYPE,
         null
     )
     for (
-        let element = iter.iterateNext();
-        element;
-        element = iter.iterateNext()
+        let anchorElement = iter.iterateNext();
+        anchorElement;
+        anchorElement = iter.iterateNext()
     ) {
-        element.click()
-        await delay(1000)
+        downloadUrls.push(anchorElement.href)
+    }
+
+    return downloadUrls
+}
+
+async function fetchBlobAndFilename(url) {
+    const response = await fetch(url)
+    const header = response.headers.get('Content-Disposition')
+    const filename = header.split(';')[1].split('=')[1]
+    const blob = await response.blob()
+
+    return [blob, filename]
+}
+
+async function dispatchClickToAllAllDownloadButtons() {
+    const downloadUrls = collectDownloadUrls()
+
+    for (const url of downloadUrls) {
+        const [blob, filename] = await fetchBlobAndFilename(url)
+        download(blob, filename)
     }
 }
 
